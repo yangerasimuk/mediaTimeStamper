dd7991c50e4f90c485acc223c01b852bdefa4eec
Yan Gerasimuk <mail@yangerasimuk.com>
2017-01-27 17:18:35 +0300

    Creation of XCode project
    Adding XCode project

---
+++ photoTimestamper/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  photoTimestamper
+//
+//  Created by Ян on 27/01/2017.
+//  Copyright © 2017 Yan Gerasimuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
